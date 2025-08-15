e0c76138a893cf2a928fe0afa2f41e02f36a385e
Yash Verma <2300033567cseh@gmail.com>
2025-08-15 11:54:39 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://3.81.77.246:9090/springapp1';
+const BASE_URL = 'http://3.91.51.29:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
